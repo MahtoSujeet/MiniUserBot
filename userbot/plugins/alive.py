a967c0edc573fef68f898676b648925738d02d1e
sujeetgh <sujeetmehta6@gmail.com>
2022-04-08 12:28:16 +0530

    modified alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -2,10 +2,26 @@ from userbot.core import minibot
 from telethon import events
 from config import Vars
 
-print("alive")
+from userbot import __version__ as minibot_ver
+from platform import python_version
+from telethon import __version__ as telever
+from datetime import datetime
 
-@minibot.on(
-    events.NewMessage(outgoing=True, pattern=f"{Vars.COMMAND_HANDLER}alive")
-)
+
+@minibot.on(events.NewMessage(outgoing=True, pattern=f"{Vars.COMMAND_HANDLER}alive"))
 async def alive(client):
-    await client.edit("**Bot is running successfully!**")
+
+    start = datetime.now()
+    await client.edit("`Checking!......`")
+    end = datetime.now()
+    ping = (end - start).microseconds / 1000
+
+    alive_msg = f"""**I am running!**
+Master: **Sujeet**
+Ping: `{ping} ms`
+MiniUserBot: `{minibot_ver}`
+Telethon version: `{telever}`
+Python version: `{python_version()}
+"""
+
+    await client.edit(alive_msg)
