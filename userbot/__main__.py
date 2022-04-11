7627e1175c8821788c19d08161a556324b680646
sujeetgh <sujeetmehta6@gmail.com>
2022-04-11 11:19:27 +0530

    added some assistant cmds

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -5,6 +5,10 @@ from .utils import plugin_manager as pm
 LOGS = logging.getLogger("MiniUserBot")
 
 LOGS.info("Preparing to Install Plugins....")
+
+# type is specified to let module loader know where to put this module
+# TODO Remove this type shit.
+pm.load_plugins_from_folder("assistant", type="assistant")
 pm.load_plugins_from_folder("plugins")
 
 miniub.start()
